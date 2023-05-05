409d1c4ae5392805886825afe9e943bbe6e8781c
Adheesh Burthia <adheeshburthia1234@gmail.com>
2023-05-05 16:51:10 +0400

    Adheesh

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home | INFINITY</title>
+    <title>HOME | INFINITY</title>
 </head>
 <body>
     <h1>This is the home page</h1>
