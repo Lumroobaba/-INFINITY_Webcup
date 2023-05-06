e20a52c107320fe99f4c67199c20d957095adfa5
Adheesh Burthia <adheeshburthia1234@gmail.com>
2023-05-06 17:57:05 +0400

    Adheesh

--- public/request.php
+++ public/request.php
@@ -1,21 +1,39 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-use Orhanerday\OpenAi\OpenAi;
+	$apiKey = "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";
+	$url = 'https://api.openai.com/v1/chat/completions';
+	
+	$headers = array(
+		"Authorization: Bearer {$apiKey}",
+		"Content-Type: application/json"
+	);
+	
+	// Define messages
+	$messages = array();
+	$message = array();
+	$message["role"] = "user";
+    $message["content"] = 'Marv is a chatbot that reluctantly answers questions with sarcastic responses:' . $_REQUEST['prompt'];
+	$messages[] = $message;
 
-$open_ai = new OpenAi('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');
+	// Define data
+	$data = array();
+	$data["model"] = "gpt-3.5-turbo";
+	$data["messages"] = $messages;
+	$data["max_tokens"] = 50;
 
-$prompt = 'Marv is a chatbot that reluctantly answers questions with sarcastic responses:' . $_POST['prompt'];
-
-$complete = $open_ai->completion([
-    'model' => 'text-davinci-002',
-    'prompt' => $prompt,
-    'temperature' => 0.5,
-    'max_tokens' => 60,
-    'top_p' => 0.3,
-    'frequency_penalty' => 0.5,
-    'presence_penalty' => 0
-]);
-
-echo $complete;
+	// init curl
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_POST, 1);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	
+	$result = curl_exec($curl);
+	if (curl_errno($curl)) {
+		echo 'Error:' . curl_error($curl);
+	} else {
+		echo $result;
+	}
+	
+	curl_close($curl);
 
 ?>
