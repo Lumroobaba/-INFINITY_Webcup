64490930578714e08cf49025d2c7cf8d0484434a
Adheesh Burthia <adheeshburthia1234@gmail.com>
2023-05-07 01:35:44 +0400

    Adheesh

--- dreams.php
+++ dreams.php
@@ -10,7 +10,7 @@
 <body>
 
   <div class="container">
-    <!-- <div class="wrapper">
+    <div class="wrapper">
       <div class="title"><span>Welcome to Celestial!</span></div>
       <div class="subtitle"><span>Let's begin your adventure</span></div>
       <form action="#" method="#">
@@ -23,8 +23,8 @@
           <input type="submit" value="Submit">
         </div>
       </form>
-    </div> -->
-    <div class="wrapper">
+    </div>
+    <!-- <div class="wrapper">
       <form action="#" method="#">
         <div class="dream">Your predictions:</div>
         <div class="row">
@@ -32,25 +32,15 @@
         </div>
       </form>
     </div>
-  </div>
+  </div> -->
 
     <!-- particles.js container --> 
     <div id="particles-js"></div> 
 
     <!-- particles.js lib - https://github.com/VincentGarreau/particles.js --> 
     <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script> 
-    <!-- stats.js lib --> <script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
+    <!-- stats.js lib --> 
+    <script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
     <script src="script.js"></script>
-    <script>
-      // Set the direction to "bottom" after 4 seconds and then to "none" after 8 seconds
-        setTimeout(function() {
-            
-        }, 4000);
-
-        setTimeout(function() {
-            
-        }, 8000);
-
-    </script>
 </body>
 </html>
